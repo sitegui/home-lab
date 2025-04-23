ac38ae416cc9d9ae2d6ab2afbcedcaff2328efdf
sitegui <2801575+sitegui@users.noreply.github.com>
2025-04-23 17:48:10 +0200

    Hash files

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod scripts;
 use crate::scripts::backup::backup;
 use crate::scripts::detect_duplicates::detect_duplicates;
 use crate::scripts::detect_films::detect_films;
+use crate::scripts::hash_files::hash_files;
 use crate::scripts::move_films::move_films;
 use crate::scripts::prepare_rename_files::prepare_rename_files;
 use crate::scripts::rename_files::rename_files;
@@ -18,6 +19,10 @@ enum Cli {
     /// Unlock the internal disk and start up the other services
     Unlock,
     Backup,
+    HashFiles {
+        path: PathBuf,
+        output: PathBuf,
+    },
     DetectDuplicates {
         path: PathBuf,
     },
@@ -44,6 +49,7 @@ fn main() -> anyhow::Result<()> {
     match Cli::parse() {
         Cli::Unlock => unlock()?,
         Cli::Backup => backup()?,
+        Cli::HashFiles { path, output } => hash_files(&path, &output)?,
         Cli::DetectDuplicates { path } => detect_duplicates(&path)?,
         Cli::PrepareRenameFiles { path, output } => prepare_rename_files(&path, &output)?,
         Cli::RenameFiles { path, input } => rename_files(&path, &input)?,
---
+++ src/scripts/hash_files.rs
@@ -0,0 +1,42 @@
+use crate::list_files;
+use sha1::digest::Output;
+use sha1::{Digest, Sha1};
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::path::Path;
+use std::{fs, io};
+
+pub fn hash_files(path: &Path, output: &Path) -> anyhow::Result<()> {
+    let mut files = Vec::new();
+    list_files::list_files(path, &mut files)?;
+
+    let total_files = files.len();
+    tracing::info!("Found {} files", total_files);
+
+    let mut paths_by_hash = BTreeMap::new();
+    for (index, file_path) in files.into_iter().enumerate() {
+        tracing::info!("Processing file {}/{}", index + 1, total_files);
+
+        match hash_file(&file_path) {
+            Err(err) => {
+                tracing::error!("Failed to read file {}: {}", file_path.display(), err);
+            }
+            Ok(hash) => paths_by_hash
+                .entry(hash)
+                .or_insert_with(Vec::new)
+                .push(file_path),
+        };
+    }
+
+    let contents = serde_json::to_string_pretty(&paths_by_hash)?;
+    fs::write(output, contents)?;
+
+    Ok(())
+}
+
+fn hash_file(file_path: &Path) -> anyhow::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut hasher = Sha1::new();
+    io::copy(&mut file, &mut hasher)?;
+    Ok(hex::encode(hasher.finalize()))
+}
