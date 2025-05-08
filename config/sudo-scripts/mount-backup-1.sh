c49c6ed272740575e3f72f4b02391fa23f0f9e48
sitegui <2801575+sitegui@users.noreply.github.com>
2025-05-08 18:02:36 +0200

    Update backup disk id

--- config/sudo-scripts/mount-backup-1.sh
+++ config/sudo-scripts/mount-backup-1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
-cryptsetup open /dev/disk/by-uuid/6f20fbbb-36ba-47dc-87b1-8ef44cdef7d3 backup-1 --key-file /home/sitegui/protected/backup-password.txt
+cryptsetup open /dev/disk/by-uuid/0b6fd7a0-ceb6-488d-ae89-835ab359c887 backup-1 --key-file /home/sitegui/protected/backup-password.txt
 mount /dev/mapper/backup-1 /home/sitegui/backup-1
