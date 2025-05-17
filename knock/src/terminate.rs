7315fdf2178fa09dd1f95d21e1fa9c55930d1b9a
sitegui <2801575+sitegui@users.noreply.github.com>
2025-05-18 00:25:41 +0200

    Graceful shutdown

--- knock/src/terminate.rs
+++ knock/src/terminate.rs
@@ -1,4 +1,6 @@
 use std::sync::{Arc, LazyLock};
+use tokio::signal::unix;
+use tokio::signal::unix::SignalKind;
 use tokio::sync::Notify;
 
 pub static TERMINATE: LazyLock<Terminate> = LazyLock::new(Terminate::new);
@@ -9,17 +11,20 @@ impl Terminate {
     fn new() -> Self {
         let notify = Arc::new(Notify::new());
 
-        let notify_clone = notify.clone();
-        tokio::spawn(async move {
-            tokio::signal::ctrl_c()
-                .await
-                .expect("failed to install CTRL+C handler");
-
-            tracing::warn!("Shutting down...");
-            notify_clone.notify_waiters();
-        });
+        tokio::spawn(Self::monitor(notify.clone(), SignalKind::interrupt()));
+        tokio::spawn(Self::monitor(notify.clone(), SignalKind::terminate()));
 
         Self(notify)
+    }
+
+    async fn monitor(notify: Arc<Notify>, signal_kind: SignalKind) {
+        unix::signal(signal_kind)
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+
+        tracing::warn!("Shutting down...");
+        notify.notify_waiters();
     }
 
     pub async fn wait(&self) {
