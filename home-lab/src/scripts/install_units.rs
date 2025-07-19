60a54f43e9b0078a03f46d86b3ec723f2f79fabf
sitegui <2801575+sitegui@users.noreply.github.com>
2025-07-19 11:19:36 +0200

    Auto-detect networks

--- home-lab/src/scripts/install_units.rs
+++ home-lab/src/scripts/install_units.rs
@@ -10,11 +10,11 @@ use std::path::{Path, PathBuf};
 use std::sync::LazyLock;
 
 pub fn install_units(force: bool, path: Option<PathBuf>) -> anyhow::Result<()> {
-    let path = path.as_deref().unwrap_or(Path::new("config"));
-    let files = if path.metadata()?.is_file() {
-        vec![path.to_owned()]
+    let target_path = path.as_deref().unwrap_or(Path::new("config"));
+    let files = if target_path.metadata()?.is_file() {
+        vec![target_path.to_owned()]
     } else {
-        list_files(path)?
+        list_files(target_path)?
     };
 
     let home = home()?;
@@ -23,48 +23,20 @@ pub fn install_units(force: bool, path: Option<PathBuf>) -> anyhow::Result<()> {
     fs::create_dir_all(&containers_dir)?;
     fs::create_dir_all(&user_dir)?;
 
-    let units = load_units(files, &containers_dir, &user_dir)?;
+    let mut units = load_units(files, &containers_dir, &user_dir)?;
     tracing::info!("Detected {} units", units.len());
 
     pull_missing_images(&units)?;
+    auto_declare_networks(&containers_dir, &user_dir, &mut units)?;
 
-    let mut updated_units = vec![];
-    for unit in units {
-        let should_update = force
-            || match fs::read_to_string(&unit.target_path) {
-                Err(err) if err.kind() == std::io::ErrorKind::NotFound => true,
-                Err(err) => return Err(err.into()),
-                Ok(contents) => contents != unit.contents,
-            };
-
-        if should_update {
-            tracing::info!("Copying {}", unit.target_path.display());
-            fs::write(&unit.target_path, &unit.contents)?;
-
-            updated_units.push(unit);
-        }
-    }
+    let updated_units = install_unit(force, units)?;
 
     if !updated_units.is_empty() {
-        Child::new("systemctl")
-            .args(["--user", "daemon-reload"])
-            .run()?;
+        enable_and_restart_services(&updated_units)?;
+    }
 
-        for unit in updated_units {
-            if let Some(enable_name) = unit.enable_name {
-                tracing::info!("Enabling {}", enable_name);
-                Child::new("systemctl")
-                    .args(["--user", "enable", &enable_name])
-                    .run()?;
-            }
-            if let Some(restart_name) = unit.restart_name {
-                tracing::info!("Restarting {}", restart_name);
-                Child::new("systemctl")
-                    .args(["--user", "restart", &restart_name])
-                    .ignore_status()
-                    .run()?;
-            }
-        }
+    if path.is_none() {
+        // TODO: remove unused units
     }
 
     Ok(())
@@ -72,25 +44,40 @@ pub fn install_units(force: bool, path: Option<PathBuf>) -> anyhow::Result<()> {
 
 fn load_units(
     files: Vec<PathBuf>,
-    containers_dir: &PathBuf,
-    user_dir: &PathBuf,
+    containers_dir: &Path,
+    user_dir: &Path,
 ) -> anyhow::Result<Vec<UnitFile>> {
-    files
-        .into_iter()
-        .filter_map(|file| {
-            let kind = match file.extension()?.to_str()? {
-                "target" => UnitKind::Target,
-                "service" => UnitKind::Service,
-                "network" => UnitKind::Network,
-                "container" => UnitKind::Container,
-                "socket" => UnitKind::Socket,
-                "timer" => UnitKind::Timer,
-                _ => return None,
-            };
+    let mut units = vec![];
+    for file in &files {
+        let kind = match file.extension().and_then(|s| s.to_str()).unwrap_or("") {
+            "target" => UnitKind::Target,
+            "service" => UnitKind::Service,
+            "network" => UnitKind::Network,
+            "container" => UnitKind::Container,
+            "socket" => UnitKind::Socket,
+            "timer" => UnitKind::Timer,
+            _ => continue,
+        };
 
-            Some(UnitFile::new(&containers_dir, &user_dir, file, kind))
-        })
-        .try_collect()
+        let name = file
+            .file_name()
+            .context("missing file name")?
+            .to_str()
+            .context("invalid file name")?
+            .to_owned();
+
+        let contents = fs::read_to_string(file)?;
+
+        units.push(UnitFile::new(
+            containers_dir,
+            user_dir,
+            kind,
+            name,
+            contents,
+        )?);
+    }
+
+    Ok(units)
 }
 
 fn pull_missing_images(units: &[UnitFile]) -> anyhow::Result<()> {
@@ -134,6 +121,94 @@ fn pull_missing_images(units: &[UnitFile]) -> anyhow::Result<()> {
     Ok(())
 }
 
+/// Detect referenced networks and declare the missing ones automatically
+fn auto_declare_networks(
+    containers_dir: &Path,
+    user_dir: &Path,
+    units: &mut Vec<UnitFile>,
+) -> anyhow::Result<()> {
+    let mut mentioned_networks = BTreeSet::new();
+    let mut existing_networks = BTreeSet::new();
+    for unit in units.iter() {
+        if unit.kind == UnitKind::Container {
+            static NETWORK_REGEX: LazyLock<Regex> =
+                LazyLock::new(|| Regex::new(r"^Network *= *(.*)\.network$").unwrap());
+
+            for line in unit.contents.lines() {
+                if let Some(capture) = NETWORK_REGEX.captures(line) {
+                    mentioned_networks.insert(capture.get(1).unwrap().as_str());
+                }
+            }
+        } else if unit.kind == UnitKind::Network {
+            existing_networks.insert(unit.name.as_str());
+        }
+    }
+
+    let missing_networks = mentioned_networks
+        .difference(&existing_networks)
+        .map(|name| name.to_string())
+        .collect_vec();
+    tracing::info!("Auto-declared {} networks", missing_networks.len());
+
+    for name in missing_networks {
+        units.push(UnitFile::new(
+            containers_dir,
+            user_dir,
+            UnitKind::Network,
+            name,
+            "[Network]\n".to_owned(),
+        )?);
+    }
+
+    Ok(())
+}
+
+fn install_unit(force: bool, units: Vec<UnitFile>) -> anyhow::Result<Vec<UnitFile>> {
+    let mut updated_units = vec![];
+
+    for unit in units {
+        let should_update = force
+            || match fs::read_to_string(&unit.target_path) {
+                Err(err) if err.kind() == std::io::ErrorKind::NotFound => true,
+                Err(err) => return Err(err.into()),
+                Ok(contents) => contents != unit.contents,
+            };
+
+        if should_update {
+            tracing::info!("Copying {}", unit.target_path.display());
+            fs::write(&unit.target_path, &unit.contents)?;
+
+            updated_units.push(unit);
+        }
+    }
+
+    Ok(updated_units)
+}
+
+fn enable_and_restart_services(updated_units: &[UnitFile]) -> anyhow::Result<()> {
+    Child::new("systemctl")
+        .args(["--user", "daemon-reload"])
+        .run()?;
+
+    for unit in updated_units {
+        if let Some(enable_name) = &unit.enable_name {
+            tracing::info!("Enabling {}", enable_name);
+            Child::new("systemctl")
+                .args(["--user", "enable", enable_name])
+                .run()?;
+        }
+        if let Some(restart_name) = &unit.restart_name {
+            tracing::info!("Restarting {}", restart_name);
+            Child::new("systemctl")
+                .args(["--user", "restart", restart_name])
+                .ignore_status()
+                .run()?;
+        }
+    }
+
+    Ok(())
+}
+
 #[derive(Debug)]
 struct UnitFile {
     target_path: PathBuf,
@@ -141,6 +216,7 @@ struct UnitFile {
     restart_name: Option<String>,
     contents: String,
     kind: UnitKind,
+    name: String,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -157,26 +233,22 @@ impl UnitFile {
     fn new(
         containers_dir: &Path,
         user_dir: &Path,
-        source_path: PathBuf,
         kind: UnitKind,
+        name: String,
+        contents: String,
     ) -> anyhow::Result<Self> {
-        let name = source_path
-            .file_name()
-            .context("missing file name")?
-            .to_str()
-            .context("invalid file name")?;
         let target_path = match kind {
-            UnitKind::Network | UnitKind::Container => containers_dir.join(name),
-            _ => user_dir.join(name),
+            UnitKind::Network | UnitKind::Container => containers_dir.join(&name),
+            _ => user_dir.join(&name),
         };
 
         let enable_name = match kind {
-            UnitKind::Service | UnitKind::Socket | UnitKind::Timer => Some(name.to_string()),
+            UnitKind::Service | UnitKind::Socket | UnitKind::Timer => Some(name.clone()),
             _ => None,
         };
 
         let restart_name = match kind {
-            UnitKind::Service | UnitKind::Timer => Some(name.to_string()),
+            UnitKind::Service | UnitKind::Timer => Some(name.clone()),
             UnitKind::Container => {
                 let base_name = name
                     .strip_suffix(".container")
@@ -192,9 +264,8 @@ impl UnitFile {
             _ => None,
         };
 
-        let contents = fs::read_to_string(&source_path)?;
-
         Ok(Self {
+            name,
             enable_name,
             restart_name,
             target_path,
